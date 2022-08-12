507e19dcbba34a8aef4e2f5ab249fccb20f92fc9
juss-10 <100239793+juss-10@users.noreply.github.com>
2022-08-12 23:56:01 +0200

    feat: update grid size with input element

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,27 @@
+const sketchElem = document.querySelector(".sketch");
+const gridRangeElem = document.querySelector("#grid-range");
 const menuContainerElem = document.querySelector(".menu-container");
 const menuItemTabElems = document.querySelectorAll(".menu-item-tab");
 
+gridRangeElem.addEventListener("input", rangeHandler)
 menuItemTabElems.forEach(menuItemElem => menuItemElem.addEventListener("click", menuHandler))
+
+function rangeHandler() {
+    updateRangeText(this.value)
+    updateGridSize(this.value)
+}
+
+function updateRangeText(value) {
+    const gridRangeTextElem = document.querySelector("#grid-range-text");
+    gridRangeTextElem.textContent = `Grid size: ${value}`;
+}
+
+function updateGridSize(gridCellCount) {
+    sketchElem.style.cssText = `
+        grid-template-columns: repeat(${gridCellCount}, ${100 / gridCellCount}%);
+        grid-template-rows: repeat(${gridCellCount}, ${100 / gridCellCount}%)
+    `;
+}
 
 function menuHandler() {
     const menuToggleElem = menuItemTabElems[0];
