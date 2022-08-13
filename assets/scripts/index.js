8cf1cec482382851614bd2456f90c7cd42a98688
juss-10 <100239793+juss-10@users.noreply.github.com>
2022-08-14 01:16:48 +0200

    feat: create grid cell divs and display cells

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,7 @@ const gridRangeElem = document.querySelector("#grid-range");
 const menuItemTabElems = document.querySelectorAll(".menu-item-tab");
 
 gridRangeElem.addEventListener("input", rangeHandler)
+gridRangeElem.addEventListener("change", gridHandler)
 menuItemTabElems.forEach(menuItemElem => menuItemElem.addEventListener("click", menuHandler))
 
 function rangeHandler() {
@@ -22,4 +23,30 @@ function updateGridSize(gridCellCount) {
         grid-template-columns: repeat(${gridCellCount}, ${100 / gridCellCount}%);
         grid-template-rows: repeat(${gridCellCount}, ${100 / gridCellCount}%)
     `;
+}
+
+function gridHandler() {
+    createGridElems()
+}
+
+function createGridElems(gridCellCount = 10) {
+    let cells = gridCellCount;
+    cells **= 2;
+    clearGrid()
+    const grid = document.createDocumentFragment();
+
+    while (cells) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell")
+        grid.append(cell)
+        --cells
+    }
+
+    sketchElem.append(grid)
+}
+
+function clearGrid() {
+    while (sketchElem.firstChild) {
+        sketchElem.firstChild.remove()
+    }
 }
