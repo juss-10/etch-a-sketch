09e951d4586d56198a5d7f6fa6df098074214442
juss-10 <100239793+juss-10@users.noreply.github.com>
2022-08-13 22:38:33 +0200

    refactor: enable modules for menu.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,7 @@
+import { menuHandler } from "./menu.js";
+
 const sketchElem = document.querySelector(".sketch");
 const gridRangeElem = document.querySelector("#grid-range");
-const menuContainerElem = document.querySelector(".menu-container");
 const menuItemTabElems = document.querySelectorAll(".menu-item-tab");
 
 gridRangeElem.addEventListener("input", rangeHandler)
@@ -21,57 +22,4 @@ function updateGridSize(gridCellCount) {
         grid-template-columns: repeat(${gridCellCount}, ${100 / gridCellCount}%);
         grid-template-rows: repeat(${gridCellCount}, ${100 / gridCellCount}%)
     `;
-}
-
-function menuHandler() {
-    const menuToggleElem = menuItemTabElems[0];
-    const currentSubmenuElem = this.parentElement.querySelector(".submenu");
-    const submenuElems = document.querySelectorAll(".submenu");
-    const hasSubmenuElem = this.parentElement.contains(currentSubmenuElem);
-    const isOpened = menuContainerElem.classList.contains("open-menu");
-
-    if (isOpened && this === menuToggleElem) {
-        submenuElems.forEach(submenu =>
-            submenu.className = "submenu hidden"
-        )
-
-        toggleMenu()
-    } else if (isOpened && hasSubmenuElem) {
-        currentSubmenuElem.classList.toggle("hidden");
-    } else if (hasSubmenuElem) {
-        this.parentElement.querySelector(".submenu").classList.toggle("hidden");
-        toggleMenu()
-    } else if (this === menuToggleElem) {
-        toggleMenu()
-    }
-    
-}
-
-function toggleMenu() {
-    toggleMobileMenu()
-    toggleDesktopMenu()
-}
-
-function toggleMobileMenu() {
-    menuItemTabElems.forEach(tabElem =>
-        tabElem.parentElement.classList.toggle("visible")
-    )
-}
-
-function toggleDesktopMenu() {
-    if (menuContainerElem.classList.contains("open-menu")) {
-        closeMenu()
-    } else {
-        openMenu()
-    }
-}
-
-function openMenu() {
-    menuContainerElem.classList.remove("close-menu")
-    menuContainerElem.classList.add("open-menu")
-}
-
-function closeMenu() {
-    menuContainerElem.classList.remove("open-menu")
-    menuContainerElem.classList.add("close-menu")
 }
---
+++ assets/scripts/menu.js
@@ -0,0 +1,57 @@
+const menuContainerElem = document.querySelector(".menu-container");
+const menuItemTabElems = document.querySelectorAll(".menu-item-tab");
+
+function menuHandler() {
+    const menuToggleElem = menuItemTabElems[0];
+    const currentSubmenuElem = this.parentElement.querySelector(".submenu");
+    const submenuElems = document.querySelectorAll(".submenu");
+    const hasSubmenuElem = this.parentElement.contains(currentSubmenuElem);
+    const isOpened = menuContainerElem.classList.contains("open-menu");
+
+    if (isOpened && this === menuToggleElem) {
+        submenuElems.forEach(submenu =>
+            submenu.className = "submenu hidden"
+        )
+
+        toggleMenu()
+    } else if (isOpened && hasSubmenuElem) {
+        currentSubmenuElem.classList.toggle("hidden");
+    } else if (hasSubmenuElem) {
+        this.parentElement.querySelector(".submenu").classList.toggle("hidden");
+        toggleMenu()
+    } else if (this === menuToggleElem) {
+        toggleMenu()
+    }
+    
+}
+
+function toggleMenu() {
+    toggleMobileMenu()
+    toggleDesktopMenu()
+}
+
+function toggleMobileMenu() {
+    menuItemTabElems.forEach(tabElem =>
+        tabElem.parentElement.classList.toggle("visible")
+    )
+}
+
+function toggleDesktopMenu() {
+    if (menuContainerElem.classList.contains("open-menu")) {
+        closeMenu()
+    } else {
+        openMenu()
+    }
+}
+
+function openMenu() {
+    menuContainerElem.classList.remove("close-menu")
+    menuContainerElem.classList.add("open-menu")
+}
+
+function closeMenu() {
+    menuContainerElem.classList.remove("open-menu")
+    menuContainerElem.classList.add("close-menu")
+}
+
+export { menuHandler }
