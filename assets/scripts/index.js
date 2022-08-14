7d6a4a5f71a7c9d5caa203efa7fb085e0c085dd4
juss-10 <100239793+juss-10@users.noreply.github.com>
2022-08-15 01:09:56 +0200

    feat: choose color mode and multiple colors

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,10 +3,15 @@ import { menuHandler } from "./menu.js";
 const sketchElem = document.querySelector(".sketch");
 const gridRangeElem = document.querySelector("#grid-range");
 const menuItemTabElems = document.querySelectorAll(".menu-item-tab");
+const colorOptionsElem = document.querySelector("#color-options");
+const colorpickerElem = document.querySelector("#colorpicker");
 let gridCellCount = 10;
+let colorMode = "default";
+let colorpickerColor;
 
 gridRangeElem.addEventListener("input", rangeHandler)
 gridRangeElem.addEventListener("change", gridHandler)
+
 menuItemTabElems.forEach(menuItemElem => menuItemElem.addEventListener("click", menuHandler))
 createGridElems(gridCellCount)
 
@@ -70,17 +75,58 @@ function enableDrawing(grid) {
 
 function draw(cell) {
     const currentBackgroundColor = window.getComputedStyle(cell).getPropertyValue("background-color");
+    let newBackgroundColor;
     let colors = currentBackgroundColor.slice(currentBackgroundColor.indexOf("(") + 1, -1).split(",");
     colors = colors.map(color => parseFloat(color.replace(" ", "")));
 
     // computed style includes 0 as alpha channel if background-color not set
     // and removes value if set to 1
-    if (colors.length === 3) {
-        return
+
+    if (colorMode === "default" && colors.length !== 3) {
+        const alpha = colors[3] + 0.1;
+        newBackgroundColor = `rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, ${alpha})`;
+    } else if (colorMode === "rainbow") {
+        newBackgroundColor = `rgb(${getRandomRGBValue()}, ${getRandomRGBValue()}, ${getRandomRGBValue()})`;
+    } else {
+        newBackgroundColor = colorpickerColor || "black";
+        console.log(newBackgroundColor)
     }
 
-    let alpha = colors[3] + 0.1;
-    const newBackgroundColor = `rgba(${colors[0]}, ${colors[1]}, ${colors[2]}, ${alpha})`;
     cell.style.backgroundColor = newBackgroundColor;
+}
 
+function getRandomRGBValue() {
+    return Math.floor(Math.random() * 256)
+}
+
+colorOptionsElem.addEventListener("change", updateColorMode)
+
+function updateColorMode() {
+    const selectedOption = this.value;
+    const colorpickerElem = document.querySelector("#colorpicker");
+    
+    if (selectedOption !== "colorpicker") {
+        colorpickerElem.classList.add("hidden");
+    }
+
+    switch (selectedOption) {
+        case "colorpicker":
+            colorpickerElem.classList.remove("hidden")
+            colorMode = "colorpicker";
+            break;
+        case "default":
+            colorMode = "default";
+            break;
+        case "rainbow":
+            colorMode = "rainbow";
+            break;
+    }
+
+    createGridElems(gridCellCount)
+}
+
+colorpickerElem.addEventListener("change", getColorpickerColor)
+
+function getColorpickerColor() {
+    colorpickerColor = this.value;
 }
